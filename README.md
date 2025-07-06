92419f1d9e9498c0d1a2d2333ebbf3345d7634f2
bryanlzl <zhenlun.bryan@gmail.com>
2025-07-07 01:21:36 +0800

    chore(readme): started including footnotes on implementations that deviate from Google's MD3 + minor TW token testing on App.tsx

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ## typical-ui repo
 
+Deviations from Google Material 3 Design:
+
+- tonal color palette var names is 3 character instead of 1-2 (e.g. tertiary should be "t" but now its "ter")
+- tonal color palette has extra tonal palettes (success "suc" & "wrn")
+
 ## ⚙️ Getting Started
 
 ```bash
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 //* APP: root react component
 const App = (): JSX.Element => {
     return (
-        <div>
-            <p>Awesome, this app runs now!</p>
+        <div data-theme="light" className="flex h-full w-full">
+            <span className="border-suc-50 mx-[4rem] my-[4rem] border">
+                <p className="text-[var(--color-primary)] shadow-[var(--elevation-1)]">Awesome, this app runs now!</p>
+            </span>
         </div>
     );
 };
